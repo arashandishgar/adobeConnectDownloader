607a2ac314c05ce1ad0e181a4793964bf98500f5
Severin Lyngstad <sly018@post.uit.no>
2019-09-12 10:02:03 +0200

    Implementing audio and video merge and file cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Main {
+    private static ArrayList<File> filesToBeDeleted;
 
     public static void main(String[] args) {
         String url = null;
@@ -15,9 +21,15 @@ public class Main {
             System.exit(-1);
         }
 
+        filesToBeDeleted = new ArrayList<>();
+
         File zipFile = downloadZipFile(url);
         unzipFiles(zipFile);
 
+        mergeMedia();
+        deleteFiles();
+
+        System.out.println("\nThe video file is output.flv");
     }
 
     public static String convertUrl(String url) throws Exception {
@@ -54,26 +66,29 @@ public class Main {
     }
 
     public static File downloadZipFile(String urlString) {
-//        try {
-//            URL website = new URL(urlString);
-//            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-//            FileOutputStream fos = new FileOutputStream("output.zip");
-//            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        try {
+            System.out.print("Downloading file: ");
+            URL website = new URL(urlString);
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("output.zip");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 
-        File zipFile = new File("output.zip");
-        System.out.println(zipFile.getName());
-        return zipFile;
+            System.out.println("success");
+            return new File("output.zip");
 
-//        } catch (MalformedURLException ex) {
-//            System.out.println("Malformed url: " + ex.toString());
-//            System.exit(-1);
-//        } catch (IOException ex) {
-//            System.out.println(ex.toString());
-//            System.exit(-1);
-//        }
+        } catch (MalformedURLException ex) {
+            System.out.println("Malformed url: " + ex.toString());
+            System.exit(-1);
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+            System.exit(-1);
+        }
+
+        return null;
     }
 
     public static void unzipFiles(File zipFile) {
+        System.out.print("Unzipping source: ");
         try {
             ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
 
@@ -101,21 +116,59 @@ public class Main {
                         fos.write(buffer, 0, lenght);
                     }
                     fos.close();
+                    filesToBeDeleted.add(newFile);
                 }
 
                 zipEntry = zis.getNextEntry();
-
-
             }
             zis.closeEntry();
             zis.close();
 
-        } catch (
-                FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        filesToBeDeleted.add(zipFile);
+        System.out.println("success");
+    }
+
+    public static void mergeMedia() {
+        System.out.print("Merging audio and video: ");
+        try {
+            Process process = Runtime.getRuntime().exec(
+                    new String[]{
+                        "cmd.exe", "/c", "start", "/wait",
+                        "ffmpeg",
+                        "-i", "audio.flv",
+                        "-i", "video.flv",
+                        "-c", "copy",
+                        "-map", "0:a:0",
+                        "-map", "1:v:0",
+                        "-shortest", "output.flv"
+                    }
+            );
+            process.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        System.out.println("success");
+    }
+
+    public static void deleteFiles() {
+        System.out.println("Deleting files: ");
+        for (File file: filesToBeDeleted) {
+            System.out.print("\t- " + file.getName());
+            if(file.delete()) {
+                System.out.println(" deleted");
+            } else {
+                System.out.println(" could not deleted");
+            }
         }
     }
 
